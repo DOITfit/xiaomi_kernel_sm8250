b9646c04dcdeabca412d2b1c3cff60d370745cd0
Mohammed Siddiq <msiddiq@codeaurora.org>
2020-09-18 15:32:35 +0530

    cnss2: Change to add prints on link down callback
    Change to add prints on link down callback.
    
    Change-Id: I7cfe38fa2185f97b4c0c2b784c143cd1acc4cca5
    Signed-off-by: Mohammed Siddiq <msiddiq@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -402,7 +402,8 @@ int cnss_pci_check_link_status(struct cnss_pci_data *pci_priv)
 	u16 device_id;
 
 	if (pci_priv->pci_link_state == PCI_LINK_DOWN) {
-		cnss_pr_dbg("%ps: PCIe link is suspended\n", (void *)_RET_IP_);
+		cnss_pr_dbg("%ps: PCIe link is in suspend state\n",
+			    (void *)_RET_IP_);
 		return -EIO;
 	}
 
@@ -2567,6 +2568,7 @@ static void cnss_pci_event_cb(struct msm_pcie_notify *notify)
 
 	switch (notify->event) {
 	case MSM_PCIE_EVENT_LINKDOWN:
+		cnss_pr_dbg("PCI link down event callback\n");
 		cnss_pci_handle_linkdown(pci_priv);
 		break;
 	case MSM_PCIE_EVENT_WAKEUP:
